8da7cc3cbb234d9fe6de73f48199c97d21e31248
Raju Kumar <rakumar@csumb.edu>
2020-11-11 21:05:12 -0800

    Initial commit

---
+++ src/CarMain.java
@@ -0,0 +1,66 @@
+class Car{
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinder, String name){
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+    public int getCylinders(){ return cylinders;}
+    public String getName(){ return name;}
+
+    public String startEngine(){
+        return "Car -> startEngine()";
+    }
+    public String accelerate(){
+        return "Car -> accelerate()";
+    }
+    public String brake(){
+        return "Car -> brake()";
+    }
+}
+
+class Mitsubishi extends Car{
+    public Mitsubishi(int cylinders, String name){
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+public class CarMain {
+    public static void main(String[] args) {
+        Car car = new Car(8, "Base Car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi = new Mitsubishi(6,"Outlander");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6,"Outlander");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+    }
+
+}
